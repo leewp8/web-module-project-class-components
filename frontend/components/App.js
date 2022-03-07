f1c94d967d661d0631f9d5e454396b2b74b3636d
William Lee <leewp8@gmail.com>
2022-03-07 18:29:24 -0500

    completed mvp

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import TodoList from './TodoList';
 import Form from './Form';
+import { date } from 'yup';
 
 
 
@@ -23,18 +24,55 @@ export default class App extends React.Component {
     }
   }
 
+
+handleAdd = (name) => {
+
+  const newTodo = {
+    name: name,
+    id: Date.now(),
+    completed: false
+  }
+
+  this.setState({
+    ...this.state,
+    todos: [...this.state.todos, newTodo]
+  })
+}
+
+handleClear = () => {
+  this.setState({
+    ...this.state,
+    todos: this.state.todos.filter(todo => {
+      return (todo.completed === false)
+    })
+  })
+}
+
+handleToggle = (clickedId) => {
+
+  this.setState({
+    ...this.state,
+    todos: this.state.todos.map(todo=> {
+      if (todo.id === clickedId) {
+        return {
+          ...todo,
+          completed: !todo.completed
+        }
+      } 
+
+      return todo;
+    })
+  })
+}
+
   render() {
     const { todos } = this.state
-    console.log(todos)
     return (
       <div>
         <h1>TO-DO LIST</h1>
-        
-        <TodoList todos={todos}/>
-
-        <Form />
-
-        <button>Clear Completed</button>
+        <TodoList handleToggle={this.handleToggle} todos={todos}/>
+        <Form handleAdd={this.handleAdd}/>
+        <button onClick={this.handleClear}>Clear Completed</button>
       </div>
     )
   }
--- frontend/components/TodoList.js
+++ frontend/components/TodoList.js
@@ -9,7 +9,7 @@ export default class TodoList extends React.Component {
         <ul>
           {
             this.props.todos.map(todo=> {
-              return (<Todo todo={todo}/>)
+              return (<Todo key={todo.id} handleToggle={this.props.handleToggle} todo={todo}/>)
             })
           }
         </ul>
