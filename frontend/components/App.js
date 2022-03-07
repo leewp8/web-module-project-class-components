21cad57978f48cf52c7e8eca3aa2a9ccbab3ee1b
William Lee <leewp8@gmail.com>
2022-03-07 17:00:56 -0500

    initial setup

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -4,7 +4,19 @@ export default class App extends React.Component {
   render() {
     return (
       <div>
-        Todo App
+        <h1>TO-DO LIST</h1>
+        <ul>
+          <li>laundry</li>
+          <li>workout</li>
+          <li>grocery shopping</li>
+        </ul>
+
+        <form>
+          <input/>
+          <button>Add</button>
+        </form>
+
+        <button>Clear Completed</button>
       </div>
     )
   }
